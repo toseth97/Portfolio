2647e78cf9da7155f15ae03c08a4bb72b41d1eec
toseth97 <oluwasheun9721@gmail.com>
2023-11-03 15:17:50 +0100

    EachPortfolio

--- src/components/EachPortfolio.js
+++ src/components/EachPortfolio.js
@@ -4,12 +4,12 @@ const EachPortfolio = ({item}) => {
    const {title,desc,link, git} = item
   return (
     
-        <div className='w-full md:w-1/2 lg:w-1/3 card_bg rounded p-4 text-black'>
-            <h1 className='text-2xl font-bold text-black text-center'>{title}</h1>
-            <p className='text-wrap text-sm text-black text-justify py-2'>{desc}</p>
+        <div className='w-full md:w-1/2 lg:w-1/3 card_bg rounded-lg p-4'>
+            <h1 className='text-2xl font-bold text-center  opacity-80'>{title}</h1>
+            <p className='text-wrap text-sm text-justify py-2 opacity-80'>{desc}</p>
             <div className='flex items-center justify-between'>
-            <a href={link} target='_blank' rel="noreferrer" className=' my-2 text-black font-bold' >Visit Site</a>
-            <a href={git} target='_blank' rel="noreferrer" className=' my-2 text-black font-bold' > Git repo</a>
+            <a href={link} target='_blank' rel="noreferrer" className=' my-2 font-bold opacity-80' >Visit Site</a>
+            <a href={git} target='_blank' rel="noreferrer" className=' my-2 font-bold opacity-80' > Git repo</a>
             </div>
         </div>
     
